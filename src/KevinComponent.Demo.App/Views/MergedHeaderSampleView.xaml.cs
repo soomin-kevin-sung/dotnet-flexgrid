e19b7ce2756d0a84c0ec82ae800dda00e3d0493f
kevin0725 <ssm0725@gmail.com>
2023-02-20 10:08:36 +0900

    Finish to Add MergedHeaderSample and Change FrozenHeaderView Class Name.

--- src/KevinComponent.Demo.App/Views/MergedHeaderSampleView.xaml.cs
+++ src/KevinComponent.Demo.App/Views/MergedHeaderSampleView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using KevinComponent.Demo.App.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KevinComponent.Demo.App.Views
@@ -17,11 +20,18 @@ namespace KevinComponent.Demo.App.Views
 	/// <summary>
 	/// MergedHeaderSample.xaml에 대한 상호 작용 논리
 	/// </summary>
-	public partial class MergedHeaderSample : Window
+	public partial class MergedHeaderSampleView : Window
 	{
-		public MergedHeaderSample()
+		public MergedHeaderSampleView()
 		{
 			InitializeComponent();
+			flexGrid.ItemsSource = Person.SampleData;
+		}
+
+		private void OnHyperlinkRequestNavigate(object sender, RequestNavigateEventArgs e)
+		{
+			var info = new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true };
+			Process.Start(info);
 		}
 	}
 }
